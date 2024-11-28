c8f4abee24b9aa7a80626134f980accdba08e62d
JEFFERSON BERNARDO SILVINO <153573631+JEFFERSON-BERNARDO@users.noreply.github.com>
2024-11-28 11:19:01 -0300

    Add files via upload

---
+++ projetos/projeto conversor de moedas/scripts.js
@@ -0,0 +1,17 @@
+ 
+ 
+ const convertButton = document.querySelector(".convent-button")
+
+ function conventValues(){
+ const inputCurrencyValue = document.querySelector(".input-currecy").value
+
+ const dolarToday = 6
+
+ const convertedValue = inputCurrencyValue/ dolarToday
+
+ console.log(convertedValue)
+
+ }
+ 
+ convertButton.addEventListener("click", conventValues)
+
