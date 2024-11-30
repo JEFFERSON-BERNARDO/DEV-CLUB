87027215d56387607e5cc1dae4a631e5fe3f7079
JEFFERSON-BERNARDO <jeffersonbernardo12@hotmail.com>
2024-11-30 09:18:08 -0300

    atualização 30/11/2024

---
+++ projetos/JAVASCRIPT/assets/Aequivo.js
@@ -0,0 +1,63 @@
+/* comentario não listado no codigo
+
+1º> variaveis
+ -let = Altera o valor quanod quiser
+ -const = valor contante/não altera o valor
+ -var = descontinuado / não usar
+
+2º> tipos de dados 
+ * strings = Textos 
+  como usar = ("aspas duplas") ('aspas simples') (`crase` chamansda de template literals ou template string) 
+  OBS: para adicionar codigo dentro de uma string adocionar> ${   }
+  
+ * Numbers = números
+   número fracionado usar ponto e n virgula, não usar aspas.
+
+3º> boolean = boleano 
+  *true = verdadeiro  
+  *false = false
+
+4º> object = objetos
+  * objetos são a junção de propriedades de um item
+  
+5º> null e undefined 
+  * null = é um campo que propositalmente pode ficar fazio.
+  * undefined = é o erro onde o codigo de programação esta vazio.
+
+6º> estrurura de dados
+  * Array = serve para organizar dados, cada dado e separado por virgula (,)
+
+7º> controle de fluxo (letra minuscula)
+  * if (SE) faça isso = if(){  } o que será executado, se if for verdadeiro.
+  * else (SE NÂO) faça aquilo = else(){  } o que será executado, se else for falso. não tem parentese.
+  * 
+
+8º> Operadores de comparação
+  * > (maior que)
+  * < (menor que)
+  * == (igual a)
+  
+9º> Functions / funções
+  = trecho de codigo que é executada somente quando solicitado = parente gente boa= usar function ---------(){} 
+
+10º> seletores
+  * getElementById = trás um elmento pelo ID
+  * getElenebtbyClassName = trás TODOS os eloementos com essa classe
+  * getElementbyTagName = trás TODOSs os elementos com essa TAG
+  * getElementbyName = trás TODOS os elementos com esse NAME
+  * querySelector = trás UM elemento, o primeiro que encontar = para achar "class (.)", "id (#)",
+  * querySelectorAll = tras TODOS os elementos que encontrar 
+
+11º> seletor html
+  * element.textContent = só html
+  * element.innerText = leva em conta o css
+  * element.innerHtml = trás tudo e permite adicionar html dentro da tag
+
+12º>Eventos
+  * todos os eventos começam com "on"
+  * 
+  *
+
+*/
+
+const convertbutton
---
+++ projetos/projeto conversor de moedas/scripts.js
@@ -0,0 +1,7 @@
+const convertButton =document.querySelector(".convent-button")
+
+function convertValues(){
+    console.log("e agora?")
+}
+
+convertButton.addEventListener ("click", convertValues)
