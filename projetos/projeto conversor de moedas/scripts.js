f63c09d7cfb07c240837075d62f4be65a560d8c3
JEFFERSON-BERNARDO <jeffersonbernardo12@hotmail.com>
2024-12-03 09:17:25 -0300

    atualização

--- projetos/projeto conversor de moedas/scripts.js
+++ projetos/projeto conversor de moedas/scripts.js
@@ -1,6 +1,24 @@
 const convertButton = document.querySelector(".convert-button");
 
 function convertValues() {
-    console.log("Agora sim!!");
+   const inputCurrencyValue= document.querySelector(".input-currency").value;
+    const currencyValueToConvert = document.querySelector(".moeda-valor-para-converter")
+    const currencyValueConverted = document.querySelector(".moeda-valor")
+  const dolarToday = 6 ;
+
+  const convertedValue = inputCurrencyValue / dolarToday;
+
+  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL"}).format(inputCurrencyValue)
+
+  currencyValueConverted.innerHTML= new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD"}).format( convertedValue)
+
+
+  console.log(convertedValue);
 }
 convertButton.addEventListener("click", convertValues);
+
+
